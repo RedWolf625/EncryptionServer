0e4d97cb58375fb4bdb26247f4500225d2b92080
Nathan <Nathan.shelby30@gmail.com>
2020-11-27 14:28:08 -0800

    Second commit

--- dec_server.c
+++ dec_server.c
@@ -35,40 +35,12 @@
 
 
 
-
-int strr = 0;       // Int that acts as a flag for the input producer to wake up the line_separator consumer
-
-int strstrp = 0;    // Int that acts as a flag for the line_separator producer to wake up the plus_sign consumer
-
-int strin = 0;      // Int that acts as a flag for the plus_sign producer to wake up the output consumer
-
-int count = 0;      // Int that indicates a string is currently being processed
-
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;      // Initialize the mutex
-
-pthread_cond_t empty = PTHREAD_COND_INITIALIZER;        // Initialize the condition variable for input
-
-pthread_cond_t string_in = PTHREAD_COND_INITIALIZER;    // Initialize the condition variable for line_separator
-
-pthread_cond_t stripped = PTHREAD_COND_INITIALIZER;     // Initialize the condition variable for plus_sign
-
-pthread_cond_t full = PTHREAD_COND_INITIALIZER;         // Initialize the condition variable for output
-
-pthread_cond_t dec5 = PTHREAD_COND_INITIALIZER;   
-
 int connectionSocket, charsRead, sentBits, sendFbits;
-
-char buffer[140020];
-
-char test[11];
 
 struct sockaddr_in serverAddress, clientAddress;
 
 socklen_t sizeOfClientInfo = sizeof(clientAddress);
 
-int pCount = 0;
-
-pid_t spawnpid;
 
 // Error function used for reporting issues
 void error(const char* msg) {
@@ -77,6 +49,8 @@ void error(const char* msg) {
 }
 
 void dec_process() {
+    char buffer[140020];
+    char test[11];
     char* key;
     char alpha[27] = { 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',' ' };
     char plainText[70000];
@@ -153,127 +127,6 @@ void setupAddressStruct(struct sockaddr_in* address,
     address->sin_addr.s_addr = INADDR_ANY;
 }
 
-/*
- Function that the takes the input from stdin, calls the helper function to put it into a buffer,
- flags the count var, and notifies the line_separator function to proceed
-*/
-void* decp_thread1(void* args)
-{
-
-        // Lock the mutex before putting item in the buffer
-        pthread_mutex_lock(&mutex);
-
-        // If we already are processing a string, don't take any more
-        if (count > 0) {
-            pthread_cond_wait(&empty, &mutex);
-        }
-
-        // Run the helper function to put the input into the temp buffer
-        dec_process();
-
-        // Unlock the mutex
-        pthread_mutex_unlock(&mutex);
-
-        // Signal to line_separater to begin and raise the flag that a string is being processed
-        pthread_cond_signal(&string_in);
-        strr++;
-        count++;
-
-    
-    return NULL;
-
-}
-
-
-/*
- Function that the takes the buffer from input, calls the helper function to replace the newline
- char, and notifies the plus_sign function to proceed
-*/
-void* decp_thread2(void* args)
-{
-
-        // Lock the mutex before checking if the buffer has data      
-        pthread_mutex_lock(&mutex);
-        while (strr == 0) {
-            // We aren't ready to strip any '\n' yet
-            pthread_cond_wait(&string_in, &mutex);
-        }
-
-        // Run the helper function to replace all '\n' with ' '
-        dec_process();
-
-        // Clear the flag for line_separator and signal the condition variable for plus_sign
-        strr--;
-        pthread_cond_signal(&stripped);
-        strstrp++;
-
-        // Unlock the mutex
-        pthread_mutex_unlock(&mutex);
-    
-    return NULL;
-}
-
-
-/*
- Function that the takes the buffer from line_separator, calls the helper function to replace ++,
- counts the string to see if we've hit 8 chars, and notifies output function to proceed
-*/
-void* decp_thread3(void* args)
-{
-
-        // Lock the mutex before checking if we are ready to proceed     
-        pthread_mutex_lock(&mutex);
-        while (strstrp == 0) {
-            // We aren't ready to strip any ++ yet
-            pthread_cond_wait(&stripped, &mutex);
-        }
-
-        dec_process();
-        strstrp--;
-        pthread_cond_signal(&full);
-
-        
-
-        // Unlock the mutex
-        pthread_mutex_unlock(&mutex);
-    
-    return NULL;
-}
-
-
-/*
- Function that takes the buffer from plus_sign and prints it to the console
-*/
-void* decp_thread4(void* args)
-{
-
-        // Lock the mutex before checking if the buffer has data      
-        pthread_mutex_lock(&mutex);
-        while (strin == 0)
-            // Buffer is empty. Wait for the producer to signal that the buffer has data
-            pthread_cond_wait(&full, &mutex);
-        
-        dec_process();
-
-        // Signal input that we are ready for the next line
-        pthread_cond_signal(&dec5);
-
-        // Unlock the mutex
-        pthread_mutex_unlock(&mutex);
-    return NULL;
-}
-
-void* decp_thread5(void* args) {
-    pthread_mutex_lock(&mutex);
-    if (pCount) {
-        pthread_cond_wait(&dec5, &mutex);
-    }
-    dec_process();
-    pthread_cond_signal(&empty);
-    count--;
-    pthread_mutex_unlock(&mutex);
-    return NULL;
-}
 
 
 // Initiate our program, create and call all 4 threads and handle their return
@@ -286,7 +139,6 @@ int main(int argc, char* argv[]) {
         exit(1);
     }
 
-    pthread_t dec_thread1, dec_thread2, dec_thread3, dec_thread4, dec_thread5;
 
     // Create the socket that will listen for connections
     int listenSocket = socket(AF_INET, SOCK_STREAM, 0);
@@ -316,49 +168,8 @@ int main(int argc, char* argv[]) {
         if (connectionSocket < 0) {
             error("ERROR on accept");
         }
-        if (pCount == 0) {
-            // If fork is successful, the value of spawnpid will be 0 in the child, the child's pid in the parent
-            pCount++;
-            printf("\nONE\n");
-            // Create the input thread
-            pthread_create(&dec_thread1, NULL, decp_thread1, NULL);
-            pthread_join(dec_thread1, NULL);
-            pCount--;
-        }
-        if (pCount == 1) {
-            pCount++;
-            // Wait for the threads to resolve
-            printf("\nTWO\n");
-            // Now create the line separator thread
-            pthread_create(&dec_thread2, NULL, decp_thread2, NULL);
-            pthread_join(dec_thread2, NULL);
-            pCount--;
-        }
-        if (pCount == 2) {
-            pCount++;
-            printf("\nTHREE\n");
-            // Now create the plus sign thread
-            pthread_create(&dec_thread3, NULL, decp_thread3, NULL);
-            pthread_join(dec_thread3, NULL);
-            pCount--;
-        }
-        if (pCount == 3) {
-            pCount++;
-            printf("\nFOUR\n");
-            // Now create the output thread
-            pthread_create(&dec_thread4, NULL, decp_thread4, NULL);
-            pthread_join(dec_thread4, NULL);
-            pCount--;
-        }
-        if (pCount == 4) {
-            pCount++;
-            printf("\nFIVE\n");
-            // Now create the output thread
-            pthread_create(&dec_thread5, NULL, decp_thread5, NULL);
-            pthread_join(dec_thread5, NULL);
-            pCount--;
-        }
-        else {}
+        
+        dec_process();
 
     }
 
--- end_server.c
+++ end_server.c
@@ -35,27 +35,6 @@
 
 
 
-
-int strr = 0;       // Int that acts as a flag for the input producer to wake up the line_separator consumer
-
-int strstrp = 0;    // Int that acts as a flag for the line_separator producer to wake up the plus_sign consumer
-
-int strin = 0;      // Int that acts as a flag for the plus_sign producer to wake up the output consumer
-
-int count = 0;      // Int that indicates a string is currently being processed
-
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;      // Initialize the mutex
-
-pthread_cond_t empty = PTHREAD_COND_INITIALIZER;        // Initialize the condition variable for input
-
-pthread_cond_t string_in = PTHREAD_COND_INITIALIZER;    // Initialize the condition variable for line_separator
-
-pthread_cond_t stripped = PTHREAD_COND_INITIALIZER;     // Initialize the condition variable for plus_sign
-
-pthread_cond_t full = PTHREAD_COND_INITIALIZER;         // Initialize the condition variable for output
-
-pthread_cond_t enc5 = PTHREAD_COND_INITIALIZER;   
-
 int connectionSocket, charsRead, sentBits, sendFbits;
 
 char buffer[140020];
@@ -66,9 +45,6 @@ struct sockaddr_in serverAddress, clientAddress;
 
 socklen_t sizeOfClientInfo = sizeof(clientAddress);
 
-int pCount = 0;
-
-pid_t spawnpid;
 
 // Error function used for reporting issues
 void error(const char* msg) {
@@ -153,127 +129,6 @@ void setupAddressStruct(struct sockaddr_in* address,
     address->sin_addr.s_addr = INADDR_ANY;
 }
 
-/*
- Function that the takes the input from stdin, calls the helper function to put it into a buffer,
- flags the count var, and notifies the line_separator function to proceed
-*/
-void* encp_thread1(void* args)
-{
-
-        // Lock the mutex before putting item in the buffer
-        pthread_mutex_lock(&mutex);
-
-        // If we already are processing a string, don't take any more
-        if (count > 0) {
-            pthread_cond_wait(&empty, &mutex);
-        }
-
-        // Run the helper function to put the input into the temp buffer
-        enc_process();
-
-        // Unlock the mutex
-        pthread_mutex_unlock(&mutex);
-
-        // Signal to line_separater to begin and raise the flag that a string is being processed
-        pthread_cond_signal(&string_in);
-        strr++;
-        count++;
-
-    
-    return NULL;
-
-}
-
-
-/*
- Function that the takes the buffer from input, calls the helper function to replace the newline
- char, and notifies the plus_sign function to proceed
-*/
-void* encp_thread2(void* args)
-{
-
-        // Lock the mutex before checking if the buffer has data      
-        pthread_mutex_lock(&mutex);
-        while (strr == 0) {
-            // We aren't ready to strip any '\n' yet
-            pthread_cond_wait(&string_in, &mutex);
-        }
-
-        // Run the helper function to replace all '\n' with ' '
-        enc_process();
-
-        // Clear the flag for line_separator and signal the condition variable for plus_sign
-        strr--;
-        pthread_cond_signal(&stripped);
-        strstrp++;
-
-        // Unlock the mutex
-        pthread_mutex_unlock(&mutex);
-    
-    return NULL;
-}
-
-
-/*
- Function that the takes the buffer from line_separator, calls the helper function to replace ++,
- counts the string to see if we've hit 8 chars, and notifies output function to proceed
-*/
-void* encp_thread3(void* args)
-{
-
-        // Lock the mutex before checking if we are ready to proceed     
-        pthread_mutex_lock(&mutex);
-        while (strstrp == 0) {
-            // We aren't ready to strip any ++ yet
-            pthread_cond_wait(&stripped, &mutex);
-        }
-
-        enc_process();
-        strstrp--;
-        pthread_cond_signal(&full);
-
-        
-
-        // Unlock the mutex
-        pthread_mutex_unlock(&mutex);
-    
-    return NULL;
-}
-
-
-/*
- Function that takes the buffer from plus_sign and prints it to the console
-*/
-void* encp_thread4(void* args)
-{
-
-        // Lock the mutex before checking if the buffer has data      
-        pthread_mutex_lock(&mutex);
-        while (strin == 0)
-            // Buffer is empty. Wait for the producer to signal that the buffer has data
-            pthread_cond_wait(&full, &mutex);
-        
-        enc_process();
-
-        // Signal input that we are ready for the next line
-        pthread_cond_signal(&enc5);
-
-        // Unlock the mutex
-        pthread_mutex_unlock(&mutex);
-    return NULL;
-}
-
-void* encp_thread5(void* args) {
-    pthread_mutex_lock(&mutex);
-    if (pCount) {
-        pthread_cond_wait(&enc5, &mutex);
-    }
-    enc_process();
-    pthread_cond_signal(&empty);
-    count--;
-    pthread_mutex_unlock(&mutex);
-    return NULL;
-}
 
 
 // Initiate our program, create and call all 4 threads and handle their return
@@ -285,8 +140,6 @@ int main(int argc, char* argv[]) {
         fprintf(stderr, "USAGE: %s port\n", argv[0]);
         exit(1);
     }
-
-    pthread_t enc_thread1, enc_thread2, enc_thread3, enc_thread4, enc_thread5;
 
     // Create the socket that will listen for connections
     int listenSocket = socket(AF_INET, SOCK_STREAM, 0);
@@ -316,49 +169,8 @@ int main(int argc, char* argv[]) {
         if (connectionSocket < 0) {
             error("ERROR on accept");
         }
-        if (pCount == 0) {
-            // If fork is successful, the value of spawnpid will be 0 in the child, the child's pid in the parent
-            pCount++;
-            printf("\nONE\n");
-            // Create the input thread
-            pthread_create(&enc_thread1, NULL, encp_thread1, NULL);
-            pthread_join(enc_thread1, NULL);
-            pCount--;
-        }
-        if (pCount == 1) {
-            pCount++;
-            // Wait for the threads to resolve
-            printf("\nTWO\n");
-            // Now create the line separator thread
-            pthread_create(&enc_thread2, NULL, encp_thread2, NULL);
-            pthread_join(enc_thread2, NULL);
-            pCount--;
-        }
-        if (pCount == 2) {
-            pCount++;
-            printf("\nTHREE\n");
-            // Now create the plus sign thread
-            pthread_create(&enc_thread3, NULL, encp_thread3, NULL);
-            pthread_join(enc_thread3, NULL);
-            pCount--;
-        }
-        if (pCount == 3) {
-            pCount++;
-            printf("\nFOUR\n");
-            // Now create the output thread
-            pthread_create(&enc_thread4, NULL, encp_thread4, NULL);
-            pthread_join(enc_thread4, NULL);
-            pCount--;
-        }
-        if (pCount == 4) {
-            pCount++;
-            printf("\nFIVE\n");
-            // Now create the output thread
-            pthread_create(&enc_thread5, NULL, encp_thread5, NULL);
-            pthread_join(enc_thread5, NULL);
-            pCount--;
-        }
-        else {}
+
+        enc_process();
 
     }
 
